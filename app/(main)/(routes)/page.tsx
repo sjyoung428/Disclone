24584d2cd793dadfb19bc6cc2291584ca5c67da1
sjyoung428 <sjy8267@naver.com>
2023-09-25 22:57:52 +0900

    feat: auth

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <Button>aa</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
