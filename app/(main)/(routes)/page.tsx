2393d100df49f24b42f448d87d5edbe58dea082a
sjyoung428 <sjy8267@naver.com>
2023-09-26 10:14:54 +0900

    feat: dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
