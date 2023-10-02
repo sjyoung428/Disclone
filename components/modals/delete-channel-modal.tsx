ffb9cd6067a7d701ab284ba84cb36faf2e77375e
sjyoung428 <sjy8267@naver.com>
2023-10-03 02:46:46 +0900

    feat: delete channel

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import qs from "query-string";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useModalStore } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModalStore();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "deleteChannel";
+  const { server, channel } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      const response = await axios.delete(url);
+
+      onClose();
+      router.refresh();
+      router.push(`/servers/${server?.id}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            채널 삭제
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            <span className="font-semibold text-indigo-500">
+              #{channel?.name}
+            </span>{" "}
+            채널을 삭제하시겠습니까?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button disabled={isLoading} onClick={onClose} variant="ghost">
+              아니오
+            </Button>
+            <Button disabled={isLoading} onClick={onClick} variant="primary">
+              예
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModalStore } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -18,6 +19,7 @@ const iconMap = {
 };
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
+  const { onOpen } = useModalStore();
   const params = useParams();
   const router = useRouter();
 
@@ -47,7 +49,10 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
             <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
           <ActionTooltip label="채널 삭제">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() => onOpen("deleteChannel", { server, channel })}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
