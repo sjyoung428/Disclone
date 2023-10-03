9e70116a5f8c21950287e1d2a1615220703a8636
sjyoung428 <sjy8267@naver.com>
2023-10-04 00:37:31 +0900

    feat: socket provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Open_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
-import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,8 +30,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
